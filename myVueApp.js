55de837e7aa12acd383df26af8d2d9aa4ad71144
brian <brian.mtr@outlook.fr>
2020-11-30 21:29:56 +0100

    add others files and others functions for films

--- myVueApp.js
+++ myVueApp.js
@@ -3,15 +3,48 @@
 new Vue({
     el:"#myapp",
     data:{
-        starwarsallfilms:[]
+        starwarsallfilms:[],
+        getAllStarWarsFilmsInfo:[],
+        getAllStarWarsFilms1:[],
+        getAllStarWarsFilms2:[]
     },
     methods:{
+        
+        //all films
         getAllStarWarsFilms(){
             fetch("https://swapi.dev/api/films/")
             .then(response=>response.json())
             .then(data=>{
                 this.starwarsallfilms=data;
             })
-        }
+        },
+        
+        //infos films
+        getAllStarWarsFilmsInfo(){
+            fetch("https://swapi.dev/api/films/")
+            .then(response=>response.json())
+            .then(data=>{
+                this.starwarsallfilmsInfo=data;
+            })
+        },
+        
+        // A new hope
+        getAllStarWarsFilms1(){
+            fetch("https://swapi.dev/api/films/1/")
+            .then(response=>response.json())
+            .then(data=>{
+                this.starwarsallfilms1=data;
+            })
+        },
+        
+        //The empire strikes back
+        getAllStarWarsFilms2(){
+            fetch("https://swapi.dev/api/films/2/")
+            .then(response=>response.json())
+            .then(data=>{
+                this.starwarsallfilms1=data;
+            })
+        },
     }
+    
 })
